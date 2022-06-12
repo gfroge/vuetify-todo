491332cafa25ee49e34bfe83ea5cc65cda952e35
unknown <>
2022-06-12 18:34:31 +0300

    1.0 deploy

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vuetify-todo/'
+    : '/'
+  ,
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 })
